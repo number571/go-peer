af5dde93bd349552deafe903b4d2a2eeffec7533
Number571 <number571@email.com>
2022-12-13 04:35:38 -0500

    update

--- cmd/hls/cmd/hls/init_app.go
+++ cmd/hls/cmd/hls/init_app.go
@@ -8,6 +8,8 @@ import (
 	"github.com/number571/go-peer/pkg/crypto/asymmetric"
 	"github.com/number571/go-peer/pkg/filesystem"
 	"github.com/number571/go-peer/pkg/types"
+
+	pkg_settings "github.com/number571/go-peer/cmd/hls/pkg/settings"
 )
 
 func initApp() (types.IApp, error) {
@@ -15,15 +17,21 @@ func initApp() (types.IApp, error) {
 		inputKey string
 	)
 
-	flag.StringVar(&inputKey, "key", "priv.key", "input private key from file")
+	flag.StringVar(&inputKey, "key", "", "input private key from file")
 	flag.Parse()
 
-	privKeyStr, err := filesystem.OpenFile(inputKey).Read()
-	if err != nil {
-		return nil, err
+	var privKey asymmetric.IPrivKey
+	switch inputKey {
+	case "":
+		privKey = asymmetric.NewRSAPrivKey(pkg_settings.CAKeySize)
+	default:
+		privKeyStr, err := filesystem.OpenFile(inputKey).Read()
+		if err != nil {
+			return nil, err
+		}
+		privKey = asymmetric.LoadRSAPrivKey(string(privKeyStr))
 	}
 
-	privKey := asymmetric.LoadRSAPrivKey(string(privKeyStr))
 	if privKey == nil {
 		return nil, fmt.Errorf("private key is invalid")
 	}
