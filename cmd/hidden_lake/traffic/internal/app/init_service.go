584762e58a4ebc731b0d936505ed985ddc919281
number571 <number571@email.com>
2023-02-09 14:05:32 +0700

    update

--- cmd/hidden_lake/traffic/internal/app/init_service.go
+++ cmd/hidden_lake/traffic/internal/app/init_service.go
@@ -16,7 +16,7 @@ func initServiceHTTP(cfg config.IConfig, db database.IKeyValueDB, connKeeper con
 	mux.HandleFunc(pkg_settings.CHandleIndexPath, handler.HandleIndexAPI())
 	mux.HandleFunc(pkg_settings.CHandleHashesPath, handler.HandleHashesAPI(db))
 	mux.HandleFunc(pkg_settings.CHandleMessagePath, handler.HandleMessageAPI(db))
-	mux.HandleFunc(pkg_settings.CHandleIndexPath, handler.HandleBroadcastAPI(db, connKeeper))
+	mux.HandleFunc(pkg_settings.CHandleBroadcastPath, handler.HandleBroadcastAPI(db, connKeeper))
 
 	return &http.Server{
 		Addr:    cfg.Address(),
