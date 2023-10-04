80090c3dcb26195b1d1fed008810d1355efde911
number571 <number571@email.com>
2023-10-05 02:21:29 +0700

    update

--- cmd/hidden_lake/messenger/_daemon/install_hlm.sh
+++ cmd/hidden_lake/messenger/_daemon/install_hlm.sh
@@ -15,7 +15,7 @@ WantedBy=multi-user.target
 
 cd /root && \
     rm -f hlm_amd64_linux && \
-    wget https://github.com/number571/go-peer/releases/download/v1.5.18/hlm_amd64_linux && \
+    wget https://github.com/number571/go-peer/releases/download/v1.5.19/hlm_amd64_linux && \
     chmod +x hlm_amd64_linux
 
 chmod +x /root/hls_amd64_linux
