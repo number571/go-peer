a6e407a99a02280d503117599bc39e6c4b1fe741
number571 <number571@email.com>
2024-01-05 02:48:40 +0700

    update

--- pkg/crypto/puzzle/puzzle_bench_test.go
+++ pkg/crypto/puzzle/puzzle_bench_test.go
@@ -9,22 +9,27 @@ import (
 
 /*
 cpu: Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz
-BenchmarkPuzzle/20-bit-12                    100         451693442 ns/op
---- BENCH: BenchmarkPuzzle/20-bit-12
-    puzzle_test.go:90: Timer: 1.267312821s
-    puzzle_test.go:90: Timer: 45.169327733s
-BenchmarkPuzzle/21-bit-12                    100         854375738 ns/op
---- BENCH: BenchmarkPuzzle/21-bit-12
-    puzzle_test.go:90: Timer: 1.881050349s
-    puzzle_test.go:90: Timer: 1m25.437556403s
-BenchmarkPuzzle/22-bit-12                    100        1746838647 ns/op
---- BENCH: BenchmarkPuzzle/22-bit-12
-    puzzle_test.go:90: Timer: 7.615185783s
-    puzzle_test.go:90: Timer: 2m54.6838481s
+BenchmarkPuzzleSequence/20-bit-12            100         463347150 ns/op
+--- BENCH: BenchmarkPuzzleSequence/20-bit-12
+    puzzle_bench_test.go:68: Timer: 1.284413537s
+    puzzle_bench_test.go:68: Timer: 46.334699469s
+BenchmarkPuzzleSequence/21-bit-12            100         852326526 ns/op
+--- BENCH: BenchmarkPuzzleSequence/21-bit-12
+    puzzle_bench_test.go:68: Timer: 2.093841036s
+    puzzle_bench_test.go:68: Timer: 1m25.232636438s
+BenchmarkPuzzleSequence/22-bit-12            100        1644269096 ns/op
+--- BENCH: BenchmarkPuzzleSequence/22-bit-12
+    puzzle_bench_test.go:68: Timer: 6.710678809s
+    puzzle_bench_test.go:68: Timer: 2m44.426894314s
+BenchmarkPuzzleSequence/23-bit-12            100        3301111606 ns/op
+--- BENCH: BenchmarkPuzzleSequence/23-bit-12
+    puzzle_bench_test.go:68: Timer: 6.587686655s
+    puzzle_bench_test.go:68: Timer: 5m30.111137125s
+PASS
 */
 
-// go test -bench=BenchmarkPuzzle -benchtime=100x
-func BenchmarkPuzzle(b *testing.B) {
+// go test -bench=BenchmarkPuzzleSequence -benchtime=100x -timeout 99999s
+func BenchmarkPuzzleSequence(b *testing.B) {
 	benchTable := []struct {
 		name     string
 		function func([]byte, uint64) uint64
@@ -40,6 +45,10 @@ func BenchmarkPuzzle(b *testing.B) {
 		{
 			name:     "22-bit",
 			function: NewPoWPuzzle(22).ProofBytes,
+		},
+		{
+			name:     "23-bit",
+			function: NewPoWPuzzle(23).ProofBytes,
 		},
 	}
 
@@ -61,29 +70,33 @@ func BenchmarkPuzzle(b *testing.B) {
 			}
 			end := time.Since(now)
 
-			b.Log("Timer:", end)
+			b.Logf("Timer (N=%d): %s", b.N, end)
 		})
 	}
 }
 
 /*
 cpu: Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz
-BenchmarkPuzzleParallel/20-bit-parallel-12                   100         212497876 ns/op
+BenchmarkPuzzleParallel/20-bit-parallel-12                   100         133510360 ns/op
 --- BENCH: BenchmarkPuzzleParallel/20-bit-parallel-12
-    puzzle_benchmark_test.go:108: Timer: 468.436922ms
-    puzzle_benchmark_test.go:108: Timer: 21.249665372s
-BenchmarkPuzzleParallel/21-bit-parallel-12                   100         348733248 ns/op
+    puzzle_bench_test.go:138: Timer: 454.959149ms
+    puzzle_bench_test.go:138: Timer: 13.351011892s
+BenchmarkPuzzleParallel/21-bit-parallel-12                   100         259375494 ns/op
 --- BENCH: BenchmarkPuzzleParallel/21-bit-parallel-12
-    puzzle_benchmark_test.go:108: Timer: 622.153059ms
-    puzzle_benchmark_test.go:108: Timer: 34.873298802s
-BenchmarkPuzzleParallel/22-bit-parallel-12                   100         853011126 ns/op
+    puzzle_bench_test.go:138: Timer: 559.685167ms
+    puzzle_bench_test.go:138: Timer: 25.937523604s
+BenchmarkPuzzleParallel/22-bit-parallel-12                   100         541665877 ns/op
 --- BENCH: BenchmarkPuzzleParallel/22-bit-parallel-12
-    puzzle_benchmark_test.go:108: Timer: 3.234952925s
-    puzzle_benchmark_test.go:108: Timer: 1m25.300982436s
+    puzzle_bench_test.go:138: Timer: 2.213373592s
+    puzzle_bench_test.go:138: Timer: 54.166558401s
+BenchmarkPuzzleParallel/23-bit-parallel-12                   100        1081891382 ns/op
+--- BENCH: BenchmarkPuzzleParallel/23-bit-parallel-12
+    puzzle_bench_test.go:138: Timer: 2.219345242s
+    puzzle_bench_test.go:138: Timer: 1m48.189016302s
 PASS
 */
 
-// go test -bench=BenchmarkPuzzleParallel -benchtime=100x
+// go test -bench=BenchmarkPuzzleParallel -benchtime=100x -timeout 99999s
 func BenchmarkPuzzleParallel(b *testing.B) {
 	benchTable := []struct {
 		name     string
@@ -100,6 +113,10 @@ func BenchmarkPuzzleParallel(b *testing.B) {
 		{
 			name:     "22-bit-parallel",
 			function: NewPoWPuzzle(22).ProofBytes,
+		},
+		{
+			name:     "23-bit-parallel",
+			function: NewPoWPuzzle(23).ProofBytes,
 		},
 	}
 
@@ -123,7 +140,7 @@ func BenchmarkPuzzleParallel(b *testing.B) {
 			}
 			end := time.Since(now)
 
-			b.Log("Timer:", end)
+			b.Logf("Timer (N=%d): %s", b.N, end)
 		})
 	}
 }
