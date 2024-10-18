470193e287671c4da5c346f050199da886cbe443
number571 <number571gg@mail.ru>
2024-10-18 22:47:32 +0700

    update

--- pkg/crypto/asymmetric/doc.go
+++ pkg/crypto/asymmetric/doc.go
@@ -1,4 +1,4 @@
-// Package asymmetric is a wrapper on Go circl packages - kyber, dilithium.
+// Package asymmetric is a wrapper on Go circl packages - mlkem768, mldsa65.
 //
 // The package also adds an additional interface for working with a list of public keys.
 package asymmetric
