5a42cf1e796634a591fae1c193f7880de3338a99
number571 <number571gg@mail.ru>
2024-10-22 14:17:12 +0700

    update

--- cmd/tools/pmanager/README.md
+++ cmd/tools/pmanager/README.md
@@ -18,8 +18,3 @@ $ go run . -salt="service-name" -work=24
 master-key
 J64mESAVm2o-8q6nWaywsoQbQvn8pf7U74O-Vr9HwSDu
 ```
-
-```bash
-$ echo "master-key" | go run . -salt="service-name" -work=24
-J64mESAVm2o-8q6nWaywsoQbQvn8pf7U74O-Vr9HwSDu
-```
--- cmd/tools/pmanager/main.go
+++ cmd/tools/pmanager/main.go
@@ -6,6 +6,8 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"os/exec"
+	"runtime"
 
 	"github.com/number571/go-peer/pkg/crypto/keybuilder"
 )
@@ -26,9 +28,53 @@ func main() {
 }
 
 func readUntilEOL() string {
-	res, _, err := bufio.NewReader(os.Stdin).ReadLine()
-	if err != nil {
+	var (
+		p = make([]byte, 0, 256)
+		b = make([]byte, 1)
+	)
+
+	if runtime.GOOS == "windows" {
+		res, _, err := bufio.NewReader(os.Stdin).ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		return string(res)
+	}
+
+	if err := exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run(); err != nil {
 		panic(err)
 	}
-	return string(res)
+	if err := exec.Command("stty", "-F", "/dev/tty", "-echo").Run(); err != nil {
+		panic(err)
+	}
+	defer func() { _ = exec.Command("stty", "-F", "/dev/tty", "echo").Run() }()
+
+	for {
+		if _, err := os.Stdin.Read(b); err != nil {
+			panic(err)
+		}
+		if b[0] == '\n' { // <enter>
+			fmt.Println()
+			break
+		}
+		if b[0] == 127 { // <backspace>
+			if len(p) == 0 {
+				continue
+			}
+			fmt.Print("\r")
+			for i := 0; i < len(p); i++ {
+				fmt.Print(" ")
+			}
+			fmt.Print("\r")
+			for i := 0; i < len(p)-1; i++ {
+				fmt.Print("*")
+			}
+			p = p[:len(p)-1]
+			continue
+		}
+		fmt.Print("*")
+		p = append(p, b[0])
+	}
+
+	return string(p)
 }
