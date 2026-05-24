c89a6ec042d9430bbe289dc6ea78d4daeb81bdb4
number571 <number571@example.com>
2026-05-24 16:18:16 +0700

    update

--- pkg/crypto/scheme/layer2/symmetric/doc.go
+++ pkg/crypto/scheme/layer2/symmetric/doc.go
@@ -13,20 +13,20 @@
 			S - pseudo random bytes (salt).
 			G - generator pseudo random bytes,
 			N - count of bytes for generator,
-	2. 	K' = HMAC( K, S ),
+	2. 	K' = KDF( K, S ),
 		where
-			K' - ...,
+			K' - one-time encryption key,
 			K  - session key,
-			HMAC - message authentication code function,
+			KDF - key derivation function,
 	3. 	C = [S, C' = E( K', P )],
 		where
 			P - plaintext,
 			C - encrypted message,
-			E - encryption function (AEAD).
+			E - encryption function (AE).
 
 	Steps of participant B:
 	4. 	FOR K IN KEY_LIST:
-			P = D( HMAC( K, S ), C' ),
+			P = D( KDF( K, S ), C' ),
 			where
 				D - decryption function.
 	5. 	IF P = NULL, than NEXT ITER (4),
